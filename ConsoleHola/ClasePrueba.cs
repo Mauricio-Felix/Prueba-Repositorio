cabb417b28a5393346d4db3b1487129cc09edd3c
mauricio1798 <70928418+mauricio1798@users.noreply.github.com>
2020-09-08 17:22:35 -0500

    Clase prueba

---
+++ ConsoleHola/ClasePrueba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleHola
+{
+    public class ClasePrueba
+    {
+        public int Edad { set; get; }
+
+        public ClasePrueba()
+        {
+
+        }
+
+        public ClasePrueba(int edad)
+        {
+            Edad = edad;
+        }
+    }
+}
